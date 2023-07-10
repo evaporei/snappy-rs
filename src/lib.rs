d19aec69c6e4e2bc3f6067d430dc19c5af90d63c
Eva Pace <eba.pachi@gmail.com>
2023-07-10 10:27:11 -0300

    add all extern functions from snappy lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,27 @@
-use libc::size_t;
+use libc::{c_char, c_int, size_t};
+
+type snappy_status = c_int;
 
 #[link(name = "snappy")]
 extern "C" {
+    fn snappy_compress(input: *const c_char,
+                       input_length: size_t,
+                       compressed: *mut c_char,
+                       compressed_length: *mut size_t) -> snappy_status;
+
+    fn snappy_uncompress(compressed: *const c_char,
+                         compressed_length: size_t,
+                         uncompressed: *mut c_char,
+                         uncompressed_length: *mut size_t) -> snappy_status;
+
     fn snappy_max_compressed_length(source_length: size_t) -> size_t;
+
+    fn snappy_uncompressed_length(compressed: *const c_char,
+                                  compressed_length: size_t,
+                                  result: *mut size_t) -> snappy_status;
+
+    fn snappy_validate_compressed_buffer(compressed: *const c_char,
+                                         compressed_length: size_t) -> snappy_status;
 }
 
 #[test]
